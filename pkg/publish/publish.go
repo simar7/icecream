0c1028b1241b746724e90f996a7a19d6dc12d40c
Simarpreet Singh <simar@linux.com>
2018-08-15 23:29:16 -0700

    pub: publish message every 1s
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- pkg/publish/publish.go
+++ pkg/publish/publish.go
@@ -1,6 +1,8 @@
 package publish
 
 import (
+	"time"
+
 	liftbridge "github.com/liftbridge-io/go-liftbridge"
 	nats "github.com/nats-io/go-nats"
 )
@@ -13,9 +15,13 @@ func Pub() {
 	defer conn.Flush()
 	defer conn.Close()
 
-	// Publish message.
-	msg := liftbridge.NewMessage([]byte("value"), liftbridge.MessageOptions{Key: []byte("key")})
-	if err := conn.Publish("foo", msg); err != nil {
-		panic(err)
+	// Publish message every 1 second
+	for {
+		msg := liftbridge.NewMessage([]byte("value"), liftbridge.MessageOptions{Key: []byte("key")})
+		if err := conn.Publish("foo", msg); err != nil {
+			panic(err)
+		}
+		time.Sleep(time.Second * 1)
 	}
+
 }
