a5bfa02aa38dd2d72335b4894c6cb1d4f99ff315
Simarpreet Singh <simar@linux.com>
2018-08-23 20:59:56 -0700

    sub: Add logic to listen for messages on the stream
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"sync"
+
+	liftbridge "github.com/liftbridge-io/go-liftbridge"
 )
 
 type EventStreamClient interface {
@@ -10,12 +12,20 @@ type EventStreamClient interface {
 }
 
 type LiftBridgeClient struct {
-	wg *sync.WaitGroup
+	wg         *sync.WaitGroup
+	servers    []string
+	streamInfo liftbridge.StreamInfo
 }
 
 func main() {
 	lbc := LiftBridgeClient{
-		wg: &sync.WaitGroup{},
+		wg:      &sync.WaitGroup{},
+		servers: []string{"localhost:9292", "localhost:9293", "localhost:9294"}, // TODO(simar7): Add cmd line flag option
+		streamInfo: liftbridge.StreamInfo{
+			Subject:           "foo",
+			Name:              "foo-stream",
+			ReplicationFactor: 3,
+		},
 	}
 	lbc.wg.Add(2)
 	go sub(lbc)
--- sub.go
+++ sub.go
@@ -1,10 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	liftbridge "github.com/liftbridge-io/go-liftbridge"
+	lift "github.com/liftbridge-io/go-liftbridge/liftbridge-grpc"
+)
 
 func (lbc LiftBridgeClient) Sub() {
 	defer lbc.wg.Done()
 	fmt.Println("real Sub() called")
+
+	// TODO(simar7): Add exponential backoff rather than panic
+	client, err := liftbridge.Connect(lbc.servers)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	if err := client.CreateStream(context.Background(), lbc.streamInfo); err != nil {
+		if err != liftbridge.ErrStreamExists {
+			panic(err)
+		}
+	}
+
+	ctx := context.Background()
+	if err := client.Subscribe(ctx, lbc.streamInfo.Subject, lbc.streamInfo.Name, func(msg *lift.Message, err error) {
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(msg.Offset, string(msg.Value))
+	}); err != nil {
+		panic(err)
+	}
+
+	<-ctx.Done()
 }
 
 func sub(lbcIface EventStreamClient) {
